4f8d4a53bf0c2646681fb0e16b341b649c6a8d26
BigECire <ericsanders5800@gmail.com>
2020-03-16 16:17:13 -0500

    Updates

---
+++ precache-manifest.965a591373ea13497c2d6730c2e2fec6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd9346a509d1c86a8c615ac872daf2b",
+    "url": "/Final-Portfolio/index.html"
+  },
+  {
+    "revision": "f5324e32fc2ab02dce1b",
+    "url": "/Final-Portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "13e5b6a28d815fac4a22",
+    "url": "/Final-Portfolio/static/js/2.c28d64fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Final-Portfolio/static/js/2.c28d64fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5324e32fc2ab02dce1b",
+    "url": "/Final-Portfolio/static/js/main.7e624964.chunk.js"
+  },
+  {
+    "revision": "bee1666f7f1f46f96b66",
+    "url": "/Final-Portfolio/static/js/runtime-main.7b0f6d2c.js"
+  },
+  {
+    "revision": "50b48d1ed57a4824951cc2596f16d267",
+    "url": "/Final-Portfolio/static/media/Fruit.50b48d1e.jpg"
+  },
+  {
+    "revision": "c48d968808cb5aabd2476749da809f82",
+    "url": "/Final-Portfolio/static/media/Hangman.c48d9688.png"
+  },
+  {
+    "revision": "bb5d30fec65686babae2e8caa53666c3",
+    "url": "/Final-Portfolio/static/media/firebase.bb5d30fe.png"
+  }
+]);
